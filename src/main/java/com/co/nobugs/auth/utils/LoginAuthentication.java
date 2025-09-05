bf8e578d83ad090e9c806c43bde28ea2b258a198
Stiven Villarraga <stivenv.ariza@gmail.com>
2025-09-05 09:53:50 -0500

    solving a login issue, the cognito login method was rewrite on LoginHandler

--- src/main/java/com/co/nobugs/auth/utils/LoginAuthentication.java
+++ src/main/java/com/co/nobugs/auth/utils/LoginAuthentication.java
@@ -37,13 +37,7 @@ public class LoginAuthentication<T> {
 
         private <A extends AuthenticationUser> InitiateAuthResponse login(A userAuthentication, CognitoService<A> cognitoService) throws NoBugsException {
             try {
-                InitiateAuthRequest authRequest = InitiateAuthRequest.builder()
-                        .clientId(cognitoService.getClientId())
-                        .authFlow(AuthFlowType.USER_PASSWORD_AUTH)
-                        .authParameters(Map.of("USERNAME", userAuthentication.getEmail(), "PASSWORD", userAuthentication.getPassword()))
-                        .build();
-
-                return cognitoService.getCognitoClient().initiateAuth(authRequest);
+                return cognitoService.login(userAuthentication);
             } catch (Exception e) {
                 throw new NoBugsException(e.getMessage(), HttpStatus.BAD_REQUEST);
             }
